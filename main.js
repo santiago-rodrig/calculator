81d1b1d0af906b6fcbb4786d0ad1eaa841b43354
Santiago <santo1996.29@gmail.com>
2019-08-16 17:54:03 -0400

    added html entities, added operands and operations

--- main.js
+++ main.js
@@ -7,18 +7,24 @@ document.addEventListener('DOMContentLoaded', () => {
   number.classList.add('number');
   operator.classList.add('operator');
   let userInput = document.getElementById('user-input');
+  let result = document.getElementById('result');
+  let operate = document.getElementById('operate');
   let num, optr;
   const digits = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
-  const operatorList = ['+', '-', '/', 'x', '^'];
+  const operatorList = ['+', '-', '/', '*', '^'];
   digits.forEach(digit => {
     num = number.cloneNode();
-    num.innerText = digit.toString();
+    num.innerHTML = digit.toString();
     numbers.append(num);
   });
   number.remove();
   operatorList.forEach(operation => {
     optr = operator.cloneNode();
-    optr.innerText = operation;
+    if (operation !== '/' && operation !== '*') {
+      optr.innerHTML = operation;
+    } else {
+      optr.innerHTML = operation === '/' ? '&divide;' : '&times;';
+    }
     operators.append(optr);
   });
   operator.remove();
@@ -30,8 +36,37 @@ document.addEventListener('DOMContentLoaded', () => {
   for (let i = 0; i < operatorGroup.length; i++) {
     operatorGroup[i].addEventListener('click', insertToDisplay);
   }
+  disableOperators();
   function insertToDisplay(e) {
     e.preventDefault();
-    userInput.innerText += this.innerText;
+    // Check if the user-input display is empty
+    if (userInput.innerText === "") {
+      enableOperators();
+    }
+    userInput.innerHTML += this.innerHTML;
+  }
+  function enableOperators() {
+    for (let i = 0; i < operatorGroup.length; i++) {
+      operatorGroup[i].disabled = false;
+    }
+  }
+  function disableOperators() {
+    for (let i = 0; i < operatorGroup.length; i++) {
+      operatorGroup[i].disabled = true;
+    }
+  }
+  operate.addEventListener('click', e => {
+    e.preventDefault();
+    let operands = obtainOperands(userInput.innerHTML);
+    let operations = obtainOperations(userInput.innnerHTML);
+    let magicArray = []; // Everything is better with magic
+  });
+  function obtainOperands(inputFromUser) {
+    return inputFromUser.split(/\+|-|&times;|&divide;|\^/);
+  }
+  function obtainOperations(inputFromUser) {
+    inputFromUser = inputFromUser.split(/\d/);
+    inputFromUser = inputFromUser.filter(foo => foo !== "");
+    return inputFromUser
   }
 });
